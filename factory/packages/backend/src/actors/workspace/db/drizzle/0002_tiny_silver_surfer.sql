bf282199b5cf2221b258b9093c9a7e52ef798a3e
Nathan Flurry <NathanFlurry@users.noreply.github.com>
2026-03-09 14:00:20 -0700

    Integrate OpenHandoff factory workspace (#212)

---
+++ factory/packages/backend/src/actors/workspace/db/drizzle/0002_tiny_silver_surfer.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `handoff_lookup` (
+	`handoff_id` text PRIMARY KEY NOT NULL,
+	`repo_id` text NOT NULL
+);
---
+++ factory/packages/client/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./backend-client.js";
+export * from "./keys.js";
+export * from "./view-model.js";
+export * from "./workbench-client.js";
